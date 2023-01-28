2c4fbd14b0e2bb6b8d18704edde5a34df92f2150
guillerglez88 <guillerglez88@gmail.com>
2023-01-28 22:42:59 +0100

    fixing normalize resource

--- nnotif-dynarest/src/libs/resource.ts
+++ nnotif-dynarest/src/libs/resource.ts
@@ -2,7 +2,14 @@ import { type Row } from "data"
 import { type Res } from "fundation"
 
 const normalize = <T extends Res>(type: string, res: Row<T>): T => {
-  return { ...res.resource, type, url: `/${type}/${res.id}` }
+  return {
+    ...res.resource,
+    type,
+    id: res.id,
+    url: `/${type}/${res.id}`,
+    created: res.created,
+    modified: res.modified,
+  }
 }
 
 export { normalize }
